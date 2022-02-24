ef0f45adb4f17760f5437c171a17fe34e0fc6cec
RahafShami2 <75248980+RahafShami2@users.noreply.github.com>
2022-02-24 23:47:22 +0200

    2

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace App4
+namespace App5
 {
     public partial class MainPage : ContentPage
     {
-
-        private void button2_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("ggg", "okkk", "f");
-        }
-
-        private void button1_Clicked(object sender, EventArgs e)
-        {
-            Browser.Source = "https://zajel.najah.edu/";
-        }
         public MainPage()
         {
             InitializeComponent();
-
         }
 
+        private void button_Clicked(object sender, EventArgs e)
+        {
+          
+            Label l = new Label();
 
+            for (int year = 1990; year <= 2030; year++)
+            {
+                l.Text = year + "\n";
+                layout.Children.Add(l);
+            }
+                 
+        }
     }
 }
+    
