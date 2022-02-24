2ff7052cac66d463a2022d5d5c8179a45acb55ad
RahafShami2 <75248980+RahafShami2@users.noreply.github.com>
2022-02-24 23:49:31 +0200

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private void button2_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("ggg", "okkk", "f");
+        }
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+            Browser.Source = "https://zajel.najah.edu/";
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+    }
+}
